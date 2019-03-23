2e274c45b66c36518d67be2acb90307ac7487fcc
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-22 22:19:56 -0400

    Create RetrofitSingleton

---
+++ app/src/main/java/com/example/searchviewwithrecyclerview/Network/RetrofitSingleton.java
@@ -0,0 +1,26 @@
+package com.example.searchviewwithrecyclerview.Network;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitSingleton {
+
+    private static final String BASE_URL = "https://raw.githubusercontent.com";
+    private static Retrofit instance;
+
+    private RetrofitSingleton(Retrofit instance) {
+        this.instance = instance;
+    }
+
+    public static Retrofit getInstance() {
+        if (instance == null) {
+            instance = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build();
+        }
+        return instance;
+    }
+}
